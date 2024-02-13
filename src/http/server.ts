d6c9a19d14bb83bb95592c9c21549445551284a2
Weber Daltro <weda95@gmail.com>
2024-02-13 19:06:14 -0300

    criação websocket

--- src/http/server.ts
+++ src/http/server.ts
@@ -2,7 +2,9 @@ import fastify from "fastify";
 import { createPoll } from "./routes/create-poll";
 import { getPoll } from "./routes/get-polls";
 import { voteOnPoll } from "./routes/vote-on-poll";
+import { pollResults } from "./ws/poll-results";
 import cookie from "@fastify/cookie";
+import websocket from "@fastify/websocket";
 
 const app = fastify();
 
@@ -11,9 +13,12 @@ app.register(cookie, {
   hook: "onRequest",
 });
 
+app.register(websocket);
+
 app.register(createPoll);
 app.register(getPoll);
 app.register(voteOnPoll);
+app.register(pollResults);
 
 app.listen({ port: 3333 }).then(() => {
   console.log("HTTP server running!");
